39a7b9c9769e04d5f273e5690905789f65480ec2
hongyver <sungje@gmail.com>
2023-02-02 14:32:57 +0900

    actix-gcd

---
+++ actix-gcd/src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+fn main() {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+    });
+
+    println!("Serving on http://localhost:30000...");
+    server
+        .bind("127.0.0.1").expect("error binding server to address")
+        .run().expect("error running server");
+}
+
+fn get_index() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(
+            r#"
+                <title>GCD Calculator</title>
+                "#,
+        )
+}
